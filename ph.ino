f8900aae4e17e28707cd62386a192df733b65b0d
fathiyaas <93133939+fathiyaas@users.noreply.github.com>
2022-07-31 12:07:08 +0700

    Add files via upload

---
+++ ph.ino
@@ -0,0 +1,21 @@
+const int potPin=34;
+float ph;
+float Value=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(potPin,INPUT);
+  delay(1000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Value= analogRead(potPin);
+  Serial.print(Value);
+  Serial.print(" | ");
+  float voltage=Value*(3.3/4095.0);
+  ph=(3.3*voltage);
+  Serial.println(ph);
+  delay(500);
+}
